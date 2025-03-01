ee7f77f4ce7ade3a4378f2b40179ba8c394ddba6
turnernator <dermi1002@protonmail.com>
2025-03-01 13:21:07 -0800

    Add Resampler class

--- sms-samp.py
+++ sms-samp.py
@@ -29,7 +29,7 @@ out_file = Path to output file [ex. /path/to/output.wav]
 notes = {'C' : 0, 'C#' : 1, 'D' : 2, 'D#' : 3, 'E' : 4, 'F' : 5, 'F#' : 6, 'G' : 7, 'G#' : 8, 'A' : 9, 'A#' : 10, 'B' : 11} # Note names
 note_re = re.compile(r'([A-G]#?)(-?\d+)') # Note Regex for conversion
 fftSize = 1024
-cache_ext = '.sc.npz' # cache file extension
+#cache_ext = '.sc.npz' # cache file extension
 
 f0Floor = 32
 f0Ceil = 1760
@@ -39,24 +39,6 @@ flags = ['g', 'B', 'G', 'P']
 flag_re = '|'.join(flags)
 flag_re = f'({flag_re})([+-]?\\d+)?'
 flag_re = re.compile(flag_re)
-
-# WAV read/write
-def read_wav(loc):
-    """Read audio files. sms-tools will give an error if their sampling rate isn't 44.1kHz and in mono.
-
-    Parameters
-    ----------
-    loc : str or file
-        Input audio file.
-
-    Returns
-    -------
-    ndarray
-        Data read from WAV file remapped to [-1, 1] and in 44.1kHz
-    """
-    fs, x = UF.wavread(loc) # sms-tools has their own wav read/write systems
-
-    return x
 
 def save_wav(loc, x):
     """Save data into a WAV file.
@@ -74,6 +56,50 @@ def save_wav(loc, x):
     None
     """
     UF.wavwrite(y, fs, loc)
+
+
+class Resampler(self, in_file, out_file, offset=0, length=1000, consonant=0, cutoff=0):
+    def init(self, in_file, out_file, offset=0, length=1000, consonant=0, cutoff=0):
+        ...
+        self.in_file = Path(in_file)
+        self.out_file = out_file
+        self.offset = float(offset)
+        self.length = int(length)
+        self.consonant = float(consonant)
+        self.cutoff = float(cutoff)
+
+        #self.render()
+
+    #def render(self):
+        #features = self.get_features
+        #self.resample(features)
+
+    #def get_features(self):
+        ### what kind does sms-tools make?
+
+    #def generate_features(self, features_path):
+    #    fs, x = UF.wavread(self.in_file) # sms-tools has their own wav read/write systems
+
+    #    return x
+
+    #def resample(self, features):
+        """Renders a WAV file using the passed WORLD features.
+
+        Parameters
+        ----------
+        features : dict
+            A dictionary of the F0, MGC, BAP, and average F0.
+
+        Returns
+        -------
+        None
+        """
+    #    if self.out_file == 'nul':
+    #        logging.info('Null output file. Skipping...')
+    #        return
+    
+    #    self.out_file = Path(self.out_file)
+    
 
 #argp = argparse.ArgumentParser()
 #argp.add_argument('-i', '--infile', type=str)
@@ -95,16 +121,6 @@ def save_wav(loc, x):
 #in_file: str = smssp.infile
 
 #out_file: str = smssp.outfile
-
-#def Render():
-#    r = open(in_file, 'rb')
-#    
-#    with open(out_file, 'xb') as R:
-#        R.write(r.read())
-#    
-#    r.close()
-
-    # copy it over to to a set location with a different wav file name.
 
 
 if __name__ == '__main__':
